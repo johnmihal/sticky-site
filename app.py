e75078302c78cbd3a2e1a0d58042581d725c1b81
Cody Thompson <cat7137@rit.edu>
2023-11-05 12:44:30 -0500

    this isnt working

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
