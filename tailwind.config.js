8ba7a0c8ab9fd53d2c2f756a065e2b4908b4c4c7
cat7137 <cat7137@rit.edu>
2023-11-05 14:03:29 -0500

    sticky notes!

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["templates/index.html", "templates/layout.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
