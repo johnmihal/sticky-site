a5fd55c869fe1e3976d77ab8c36b75af1294e8bf
John Mihal <johnmihal34@gmail.com>
2023-11-05 12:22:44 -0500

    removed unneccessary tool bars from ref

--- reference-material/pen-export-porpeoJ/dist/script.js
+++ reference-material/pen-export-porpeoJ/dist/script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
+// const toolbar = document.getElementById('toolbar');
 const ctx = canvas.getContext('2d');
 
 const canvasOffsetX = canvas.offsetLeft;
@@ -13,22 +13,22 @@ let lineWidth = 5;
 let startX;
 let startY;
 
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-});
+// toolbar.addEventListener('click', e => {
+//     if (e.target.id === 'clear') {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     }
+// });
 
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
+// toolbar.addEventListener('change', e => {
+//     if(e.target.id === 'stroke') {
+//         ctx.strokeStyle = e.target.value;
+//     }
 
-    if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
-    }
+//     if(e.target.id === 'lineWidth') {
+//         lineWidth = e.target.value;
+//     }
     
-});
+// });
 
 const draw = (e) => {
     if(!isPainting) {
