ab7a441fe2b5e7cc2e9a791336b6f5bee218c345
John Mihal <johnmihal34@gmail.com>
2023-12-21 00:23:32 -0500

    setting up routes, db.
    Im sorta tie-ing everything together

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    client = MongoClient("url goes here")
+    db = client.test_database
+    collection = db.test_collection
+
+
+    return render_template("index.html")
+
+@app.route('//post',methods=['POST'])
+def post():
     return render_template("index.html")
---
+++ static/draw-script.js
@@ -0,0 +1,65 @@
+const canvas = document.getElementById('drawing-board');
+const clearBtn = document.getElementById('clearBtn');
+const exportBtn = document.getElementById('exportBtn');
+const loadBtn = document.getElementById('loadBtn');
+
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.getBoundingClientRect()["x"];
+const canvasOffsetY = canvas.getBoundingClientRect()["y"];
+
+console.log(canvasOffsetX);
+
+// canvas.width = window.innerWidth - canvasOffsetX;
+// canvas.height = window.innerHeight - canvasOffsetY;
+
+canvas.width = 500;
+canvas.height = 500;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+ctx.fillStyle = '#ffe599';
+
+clearBtn.addEventListener('click', e => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+saveBtn.addEventListener('click', e => {
+    console.log("save")
+    let sticky_data = canvas.toDataURL("image/png", 1);
+    console.log(sticky_data)
+});
+
+// loadBtn.addEventListener('click', e => {
+//     alert("not implemented");
+// });
+
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX-canvasOffsetX , e.clientY-canvasOffsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
