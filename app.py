5dab3b29c2dfec4c4c255a678af90d92e7e1a3cf
John Mihal <johnmihal34@gmail.com>
2024-02-13 18:40:56 -0500

    post works with db

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from pymongo import MongoClient
-import secrets
+import secrets_1
+import datetime
 
 app = Flask(__name__)
-
+client = MongoClient(secrets_1.mongodblink)
+db = client.CoolDatabaseName
+collection = db.CoolCollectionName
 
 @app.route("/",methods=['GET'])
 def home():
-    client = MongoClient(secrets.mongodblink)
-    db = client.CoolDatabaseName
-    collection = db.CoolCollectionName
+
 
 
     return render_template("index.html")
@@ -17,4 +18,10 @@ def home():
 @app.route('/',methods=['POST'])
 def post():
     print("i recived a post request!")
+    content = request.json
+    content["date"] = datetime.datetime.now(tz=datetime.timezone.utc)
+    print(content)
+
+    img_obj = collection.insert_one(content).inserted_id
+    print(img_obj)
     return render_template("index.html")
